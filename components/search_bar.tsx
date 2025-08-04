f73238842ee92185afc83467d8a6ececc40764d5
Christian Lee-Flinders <cleeflinders@gmail.com>
2025-08-04 21:56:59 +0100

    ADD: Services, connection to NMDB for movies API

---
+++ components/search_bar.tsx
@@ -0,0 +1,25 @@
+import { icons } from '@/constants/icons'
+import React from 'react'
+import { Image, TextInput, View } from 'react-native'
+
+interface props {
+  placeholder: string,
+  onPress?: () => void,
+}
+
+const SearchBar = ({ placeholder, onPress}: props) => {
+  return (
+    <View className='flex-row items-center rounded-full px-5 py-4 bg-dark-200'>
+      <Image source={icons.search} resizeMode='contain' tintColor='#ab8bff' className='size-5'/>
+       <TextInput 
+        onPress={onPress} 
+        placeholder={placeholder} 
+        value="" 
+        onChangeText={() => {}} 
+        placeholderTextColor='#8ab55db' 
+        className="flex-1 ml-2 text-white" />
+    </View>
+  )
+}
+
+export default SearchBar
